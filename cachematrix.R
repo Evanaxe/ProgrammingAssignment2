2e8cb921862cdb519e4401d00cccf119f3493f70
Evanaxe <Evanaxe@streetwisemail.com>
2015-02-22 17:01:45 -0500

    Commit Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the following function creates a list of 4 components
+## Each component is a function (set, get, setInverse and getInverse)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  inverse = NULL
+  set = function(y)
+  {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  get = function() x
+  
+  setInverse = function(inverse) inverse <<- inverse
+  
+  getInverse = function() inverse
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
+# The following function calcuates the inverse of the matrix if it is not in the
+# cache yet. It will skip the calculation if the inverse is already in the cache
+# Function solve is used to compute the inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+   
+  inverse = x$getInverse()
+  
+  if(!is.null(inverse))
+  {
+    # Indicating cached data is being retrieved
+    message("Getting cached data")
+    return(inverse)
+  }
+
+  data = x$get()
+  
+  inverse = solve(data, ...)
+  x$setInverse(inverse)
+  inverse
+
 }
